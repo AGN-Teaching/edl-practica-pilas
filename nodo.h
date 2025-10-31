3b59f9deee8901eac1f897ad71f375a639dc0d15
Abel García Nájera <abel.garcia.n@gmail.com>
2025-10-31 12:24:38 -0600

    Refactor Nodo class constructor and methods

--- nodo.h
+++ nodo.h
@@ -1,13 +1,37 @@
+#ifndef NODO_H
+#define NODO_H
+
 template <typename T>
 class Nodo {
 public:
-    Nodo();
+    Nodo(T x, Nodo<T> *sig);
+    T getElemento() const;
+    Nodo<T> *getSiguiente() const;
+
+private:
     T elemento;
     Nodo<T> *siguiente;
+    template <typename U>
+    friend class Pila;
 };
 
 
 template <typename T>
-Nodo<T>::Nodo() {
-    siguiente = NULL;
+Nodo<T>::Nodo(T x, Nodo<T> *sig) {
+    elemento = x;
+    siguiente = sig;
 }
+
+
+template <typename T>
+T Nodo<T>::getElemento() const {
+    return elemento;
+}
+
+
+template <typename T>
+Nodo<T> *Nodo<T>::getSiguiente() const {
+    return siguiente;
+}
+
+#endif // NODO_H
